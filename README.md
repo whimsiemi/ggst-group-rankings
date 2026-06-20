d9949266e3de4b9f807a540a081933df3040f21c
whimsiemi <mochanitrate@gmail.com>
2026-06-20 19:53:19 +0100

    initial

---
+++ README.md
@@ -0,0 +1,15 @@
+# GGST - Silli Leaderboards
+
+## DESCRIPTION 
+Make an online leaderboard featuring a group of players, from friends and local scene members to national pros and even just your collection of alts!
+
+## SCOPE
+This repository is non-functional beyond the command line so far, but it will have the following features when it is finished and available on a website:
+- A user interface to create groups of players using an optional display name, their puddle-farm ID and the character they play
+- A webpage index for your leaderboard, on which you can refresh the ratings (with a 60-second cooldown) or edit the players in the group (if you created the group on this IP address)
+
+## CURRENT PROGRESS (JS)
+The backend stuff for making the list itself is fully functional, but I may wanna make a more accessible way to create datasets through the CLI (as well as add the ability to choose which dataset to use)
+
+## CURRENT PROGRESS (WEB)
+Haven't started at all... I'm honestly incredibly daunted by this, as I will need to learn HTML in order to make something not only functional but also presentable
---
+++ fetch_rankings.js
@@ -0,0 +1,40 @@
+const axios = require("axios");
+const players = require("./datasets/guildford.json");
+
+async function fetch_players(json) {
+  let player_arr = [];
+  for (let i in json) {
+    await axios
+      .get("https://puddle.farm/api/player/" + json[i].id)
+      .then((response) => {
+        function get_char_index() {
+          for (let j in response.data.ratings) {
+            if (response.data.ratings[j].char_short == json[i].main) return j;
+          }
+        }
+        let char_index = get_char_index();
+        player_arr.push({
+          name: json[i].name,
+          char: response.data.ratings[char_index].character,
+          rating: response.data.ratings[char_index].rating,
+        });
+      });
+  }
+  player_arr.sort((a, b) => b.rating - a.rating);
+  for (let i in player_arr) {
+    let ranking = "";
+    if (
+      player_arr[i].rating.toString().startsWith("1000") &&
+      player_arr[i].rating.toString().length > 5
+    ) {
+      ranking = player_arr[i].rating.toString().slice(4) + " DR";
+    } else {
+      ranking = player_arr[i].rating + " RP";
+    }
+    console.log(
+      player_arr[i].name + " (" + player_arr[i].char + ", " + ranking + ")",
+    );
+  }
+}
+
+list = fetch_players(players);
